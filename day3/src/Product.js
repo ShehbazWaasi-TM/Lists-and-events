2b4c60d69b51fae8d1db2c4fd0240ad4457ceb96
ShehbazWaasi20 <shehbazwaasi@gmail.com>
2022-07-18 15:31:33 +0530

    task6

--- day3/src/Product.js
+++ day3/src/Product.js
@@ -1,34 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import productsdata from './products.json';
-
-const Product = (props) => {
-    // const [filterProducts, setFilterPRoducts] = useState([...productsdata]);
-    
-
-    
-
+const Product = ({title='',category=''}) => {
+    // const [filterProducts, setFilterPRoducts] = useState([...productsdata]))
+    // const [toggle,setToggle] = useState(false)
+    // const [sortdata,setSortdata] = useState()
+ 
+    // // console.log("sortdata" , sortdata)
+    // const [filteredRes, setFilteredRes] = useEffect()
     const filteredData = productsdata.filter((el)=> {
-        console.log(el)
-        if (props.title === "" && props.category===""){
-            // return setFilterPRoducts(el);
-            return el
+        if(title!==""){
+            return (el.title.toLowerCase().includes(title))
         }
-        if(props.title!==""){
-            return (el.title.toLowerCase().includes(props.title))
-            // return setFilterPRoducts(el.title.toLowerCase().includes(props.title))
+        if(category!==""){
+            return (el.category.toLowerCase().includes(category))
         }
-        if(props.category!==""){
-            return (el.category.toLowerCase().includes(props.category))
-            // return setFilterPRoducts(el.category.toLowerCase().includes(props.category))
-        }
-        return filteredData
+        return el;
     })
-    console.log(filteredData)
+    // setFilteredRes(filteredData)
+    const [tableData, setTableData] = useState(filteredData)
+    console.log("filtereddata", filteredData)
+    useEffect(()=>{
+        setTableData(filteredData)
+    },[])
     //sortedtitle
     const sortedtitle =() =>{
+        // setToggle(true)
         const sortproduct = [...filteredData].sort((a,b)=>(a.title<b.title?-1:1))
         console.log("sortedproduct " , sortproduct)
-
+        // setsortdata(sortproduct);
+        setTableData(sortproduct);
     }
     const submitevent = ()=>{
         filteredData.map((x)=>(console.log(x.title)))
@@ -48,7 +48,7 @@ const Product = (props) => {
                 </tr>
             </thead>
             <tbody>
-                {filteredData.map((product)=>(
+                {tableData.map((product)=>(
                     <tr key={product.id}>
                         <td>{product.id}</td>
                         <td>{product.title}</td>
